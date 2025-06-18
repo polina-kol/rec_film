c23cec618c1e28375fd6741fd16624498bfc5576
polina-kol <kolokolovapolina3@gmail.com>
2025-06-18 13:14:35 +0300

    f

--- app.py
+++ app.py
@@ -10,17 +10,14 @@ df = load_data()
 
 st.title("Случайные фильмы с постерами")
 
-# Инициализируем состояние с выбранными фильмами
-if "sample_df" not in st.session_state:
-    st.session_state.sample_df = df.sample(10).reset_index(drop=True)
+# Кнопка для загрузки случайных фильмов
+if st.button("Показать 10 случайных фильмов"):
+    # Выбираем 10 случайных фильмов
+    sample_df = df.sample(10).reset_index(drop=True)
 
-# Кнопка обновления списка фильмов
-if st.button("Показать другие 10 фильмов"):
-    st.session_state.sample_df = df.sample(10).reset_index(drop=True)
-
-# Отображаем фильмы
-for i, row in st.session_state.sample_df.iterrows():
-    st.subheader(row['movie_title'])
-    st.image(row['image_url'], width=200)
-    st.write(row.get('description', 'Описание отсутствует'))
-    st.markdown("---")
+    # Отображаем фильмы
+    for i, row in sample_df.iterrows():
+        st.subheader(row['movie_title'])
+        st.image(row['image_url'], width=200)
+        st.write(row.get('description', 'Описание отсутствует'))
+        st.markdown("---")
